88b08df119b2288235eb5410eed514c00eee263c
Leg0l4s <alessandrodicamillo0@gmail.com>
2025-10-15 14:30:48 +0200

    fix cors

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://lighthearted-daifuku-1601b9.netlify.app'],
   credentials: true
 }));
 app.use(express.json());
